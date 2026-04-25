e179b0665e73641f93700ea89c664cd82bf3b2e6
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-25 13:12:02 +0530

    feat(auth): add login validation schemausing yup

--- src/features/auth/validations/auth.validation.js
+++ src/features/auth/validations/auth.validation.js
@@ -1,6 +1,6 @@
 import * as yup from "yup"
 
 export const loginSchema= yup.object({
-email: yup.string().email().required("Email is required"),
-pasword: yup.string().min(6).required("Password is required"),
+email: yup.string().email("Invalid email").required("Email is required"),
+pasword: yup.string().min(8, "minimum 8 character").required("Password is required"),
 })
