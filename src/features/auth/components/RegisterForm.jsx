467a2805c295dd9e8ba84a1a74db61ed092bc9c5
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-26 16:14:25 +0530

    feat(auth): implement register form with validation

---
+++ src/features/auth/components/RegisterForm.jsx
@@ -0,0 +1,54 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { registerSchema } from '../validations/auth.validation'
+import { Input } from '@/shared/components/ui/input'
+import { Label } from '@/shared/components/ui/label'
+import { Button } from '@/shared/components/ui/button'
+
+const RegisterForm = ({onSubmit}) => {
+	const {register, handleSubmit, formState: {errors, isSubmitting}}= useForm({
+		resolver: yupResolver(registerSchema),
+	})
+  return (
+	<form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+		<div className="space-y-1">
+			<Label>FULL NAME</Label>
+			<Input placaholder="Abdul fathah" {...register("name")}/>
+			{errors?.fullName && (
+				<p className='text-red-500 text-sm'>{errors.fullName.message}</p>
+			)}
+		</div>
+
+		<div className='space-y-1'>
+			<Label>EMAIL</Label>
+			<Input placaholder="abdulfathah@gmail.com" {...register("email")}/>
+			{errors?.email && (
+				<p className='text-red-500 text-sm'>{errors.email.message}</p>
+			)}
+		</div>
+
+		<div className='space-y-1'>
+			<Label>PASSWORD</Label>
+			<Input placaholder="........" {...register("password")}/>
+			{errors?.password && (
+				<p className='text-red-500 text-sm'>{errors.password.message}</p>
+			)}
+		</div>
+
+		<div className='space-y-1'>
+			<Label>CONFIRM PASSWORD</Label>
+			<Input placaholder="........" {...register("confirmPassword")} />
+			{errors?.confirmPassword && (
+				<p className='text-red-500 text-sm'>{errors.confirmPassword.message}</p>
+			)}
+		</div>
+
+		<Button type="submit" className="w-full">
+			{isSubmitting ? "Creating account..." : "Create Account"}</Button>
+
+	</form>
+  )
+}
+
+export default RegisterForm
