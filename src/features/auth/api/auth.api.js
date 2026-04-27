82137c245e199c4962de1d7503dad1d85f6ad768
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-27 20:24:20 +0530

    feat(auth): add authentication API services

--- src/features/auth/api/auth.api.js
+++ src/features/auth/api/auth.api.js
@@ -1,4 +1,14 @@
-export const loginApi= async(data)=>{
-	const res= await api.post ("/auth/login", data)
-	return res.data
-}
+import api from "@/services/axios"
+
+
+export const registerApi= (data)=> api.post("/auth/register", data)
+
+export const verifyEmailApi= (data)=> api.post("/auth/verify-email", data)
+
+export const loginApi= (data)=> api.post ("/auth/login", data)
+
+export const forgotPassword = (data)=> api.post("/auth/forgot-password", data)
+
+export const verifyOtp= (data)=> api.post("/auth/verify-otp", data)
+
+export const resetPassword= (data)=> api.post("/auth/reset-password", data)
