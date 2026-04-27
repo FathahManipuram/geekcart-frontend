f20ecd885de213adb0ca3327736a9bb03f745a10
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-27 18:02:35 +0530

    feat(auth): add reset password route

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,6 +1,7 @@
 import ForgotPasswordPage from "@/features/auth/pages/ForgotPasswordPage";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import RegisterPage from "@/features/auth/pages/RegisterPage";
+import ResetpassordPage from "@/features/auth/pages/ResetpassordPage";
 import VerifyOtpPage from "@/features/auth/pages/VerifyOtpPage";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -22,6 +23,10 @@ export const router= createBrowserRouter([
 	{
 		path: "/verify-otp",
 		element: <VerifyOtpPage/>
+	},
+	{
+		path: "/reset-password",
+		element: <ResetpassordPage/>
 	}
 ])
 
