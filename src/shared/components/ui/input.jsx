b1d7526f001d5e41935b8eb009c684d6062d7fe6
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-29 19:33:17 +0530

    fix(auth): fixed input and label conflict

--- src/shared/components/ui/input.jsx
+++ src/shared/components/ui/input.jsx
@@ -13,6 +13,10 @@ function Input({
       data-slot="input"
       className={cn(
         "h-15 w-full min-w-0 border-b-2 bg-transparent py-5 text-base transition-[color,box-shadow, background-color] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground placeholder:text-[16px] placeholder:font-normal disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+<<<<<<< HEAD
+=======
+
+>>>>>>> 3bb96d0 (fix(auth): fixed input and label conflict)
         className
       )}
       {...props} />
