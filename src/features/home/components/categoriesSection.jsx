9b94b6224c14b63132cabd36c37ac97774ed693f
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-29 18:47:10 +0530

    feat(home): implement category section

--- src/features/home/components/categoriesSection.jsx
+++ src/features/home/components/categoriesSection.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const categoriesSection = () => {
   return (
-	<div>
-	  
-	</div>
+	<section>
+		<h2 className='text-lg font-semibold mb-4'>
+			Curated categories
+		</h2>
+	</section>
   )
 }
 
