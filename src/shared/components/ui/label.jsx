0354e1a33f81904f4c8f2c4bfe5a0bd1a2b75e85
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-29 20:24:13 +0530

    fix(auth): fixed input and label conflict

--- src/shared/components/ui/label.jsx
+++ src/shared/components/ui/label.jsx
@@ -11,12 +11,8 @@ function Label({
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-<<<<<<< HEAD
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-=======
         "flex items-center gap-2 text-[10px] leading-none font-bold select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
->>>>>>> feature/auth-system
-        className
+       className
       )}
       {...props} />
   );
