9a291f493b3a6b5d7bdbf8d5a5f7f621f49f97b6
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-04-29 18:32:59 +0530

    feat(home): add hero section UI

--- src/features/home/components/HeroSection.jsx
+++ src/features/home/components/HeroSection.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-	<div>
-	  
-	</div>
+	<section className='relative w-full h-[300px] md:h-[400px] bg-[#d6b28d] flex items-center justify-center overflow-hidden'>
+		<h1 className='absolute text-[60px] md:text-[100px] font-extrabold text-white30 text-center leading-none'>
+			PREMIUM <br/> COLLECTION
+		</h1>
+
+		<img src="" alt="hero" 
+		className='relative z-10 h-[250px md:h-[350px] object-contain'/>
+	</section>
   )
 }
 
