248b6fe1aedbfacb5a17ca4f3b3eeb4a00e51f9f
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-07-08 19:35:32 +0530

    feat: update environment configuration in .env.example and env.js; enhance .gitignore to include environment files

--- src/shared/config/env.js
+++ src/shared/config/env.js
@@ -1,4 +1,6 @@
-export const ENV={
-	API_URL: import.meta.env.VITE_API_URL,
-	APP_NAME: import.meta.env.VITE_APP_NAME,
-}
+export const ENV = {
+  API_URL: import.meta.env.VITE_API_URL,
+  APP_NAME: import.meta.env.VITE_APP_NAME,
+  GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  RAZORPAY_KEY_ID: import.meta.env.VITE_RAZORPAY_KEY_ID,
+};
