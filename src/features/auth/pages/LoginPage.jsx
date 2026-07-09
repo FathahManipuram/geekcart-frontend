fc57c3cc8e9351e06d585f8d01dab71e62c2f88a
Fathah Manipuram <abdulfathahmtmpm@gmail.com>
2026-07-09 21:47:14 +0530

    feat: refactor LoginPage layout for improved readability and maintainability

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -32,53 +32,67 @@ const LoginPage = () => {
 		
 	}
   return (
-	<div className='min-h-screen flex items-center justify-center bg-background'>
-	  <Card className="w-120 shadow-lg rounded-none p-8">
-		<CardHeader>
-			<CardTitle className="text-3xl font-extrabold">
-				Welcome Back
-			</CardTitle>
-			<p className='text-sm text-muted-foreground'>Please enter your details to access your account.</p>
-		</CardHeader>
-		<CardContent>
-			<LoginForm onSubmit={handleLogin}/>
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <Card className="w-120 shadow-lg rounded-none p-8">
+        <CardHeader>
+          <CardTitle className="text-3xl font-extrabold">
+            Welcome Back
+          </CardTitle>
+          <p className="text-sm text-muted-foreground">
+            Please enter your details to access your account.
+          </p>
+        </CardHeader>
+        <CardContent>
+          <LoginForm onSubmit={handleLogin} />
 
-			<div className="flex items-center gap-4 my-6">
-  <div className="flex-1 h-px bg-border/60" />
-  
-  <span className="text-xs tracking-widest text-muted-foreground uppercase">
-    Or continue with
-  </span>
-  
-  <div className="flex-1 h-px bg-border/60" />
-</div>
+          <div className="flex items-center gap-4 my-6">
+            <div className="flex-1 h-px bg-border/60" />
 
-				<GoogleLogin shape='circle' onSuccess={async(credentialResponse)=>{
-					try{
-						console.log("googllogCredential",credentialResponse)
-						const token= credentialResponse.credential;
-						console.log("TOKen", token)
-						await loginWithGoogle(token)
-						toast.success("Google login successful")
-						navigate("/")
-					} catch(err){
-						console.log(err)
-						toast.error(err?.response?.data?.message|| "Google login failed")
-					}
-				}}
-				onError={()=>{
-					console.log("Google login failed")
-				}} className='w-full border rounded-lg py-2 flex items-center justify-center gap-2 mb-8'
-				/>
+            <span className="text-xs tracking-widest text-muted-foreground uppercase">
+              Or continue with
+            </span>
 
+            <div className="flex-1 h-px bg-border/60" />
+          </div>
 
-				<p className='text-center font-light text-xs mt-8'>New to GeekCart? {" "}
-					<Link to="/register" className='text-primary cursor-pointer font-bold'>Create an account</Link>
-				</p>
-		</CardContent>
-	  </Card>
-	</div>
-  )
+          <div >
+            <GoogleLogin
+              shape="circle"
+              onSuccess={async (credentialResponse) => {
+                try {
+                  console.log("googllogCredential", credentialResponse);
+                  const token = credentialResponse.credential;
+                  console.log("TOKen", token);
+                  await loginWithGoogle(token);
+                  toast.success("Google login successful");
+                  navigate("/");
+                } catch (err) {
+                  console.log(err);
+                  toast.error(
+                    err?.response?.data?.message || "Google login failed",
+                  );
+                }
+              }}
+              onError={() => {
+                console.log("Google login failed");
+              }}
+              className="w-full border rounded-lg py-2 flex items-center justify-center gap-2 mb-8"
+            />
+          </div>
+
+          <p className="text-center font-light text-xs mt-8">
+            New to GeekCart?{" "}
+            <Link
+              to="/register"
+              className="text-primary cursor-pointer font-bold"
+            >
+              Create an account
+            </Link>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
 export default LoginPage
